68d0380964ec3ffb03e4d8cc6501f7ed17af0ba5
Malik Kennedy <mksybr@gmail.com>
2020-11-10 00:33:58 -0800

    Click to turn todo list items grey, BUG: add an item to reset it

--- src/rum_workshop/scratch.cljs
+++ src/rum_workshop/scratch.cljs
@@ -7,15 +7,19 @@
 
 (def my-name (atom ""))
 
-(def db (atom {:input-text ""}))
+(def db (atom {:input-text ""
+               :todo-list []}))
 
 (def input-text (rum/cursor-in db [:input-text]))
+
+(def todo-list (rum/cursor-in db [:todo-list]))
 
 (def root-element (.getElementById js/document "app"))
 
 (rum/defc input-text-area < rum/reactive 
   []
-  [:input 
+  [:div
+   [:input 
     {:value (rum/react input-text)
      :on-change (fn [e] (reset! input-text (.. e -target -value)))
      :on-key-down 
@@ -23,14 +27,37 @@
        (js/console.log (.-keyCode e) @input-text)
        (when (and (= (.-keyCode e) enter-key-code)
                (seq @input-text))        
-         (reset! input-text "")))}])
+         (do       
+           (swap! todo-list conj @input-text)
+           (reset! input-text ""))))}]
+   [:button
+   {:on-mouse-down
+    (fn [e]
+      (.preventDefault e)
+      (swap! todo-list conj @input-text)
+      (reset! input-text ""))} 
+    "Add to list!"
+    ]])
+
+(rum/defc todo-list-items < rum/reactive
+  []
+  [:ul
+   (for [todo @todo-list]
+     [:li 
+      {:key (gensym)
+       :on-mouse-down
+       (fn [e]
+         (js/console.log #_(-> .-color .-style .-target)   
+           (set! (-> e .-target .-style .-color) "#ccc")))}      
+       todo])])
 
 (rum/defc app < rum/reactive
   []
   [:div 
    (input-text-area)
+   (todo-list-items)
    [:p 
     {:value (rum/react input-text)}
-    (str "Hello, "@input-text)]])
+    @input-text]])
 
 (rum/mount (app) root-element)
