85ec23b9219f1a14434319538d26482f86d8ed1e
Malik Kennedy <mksybr@gmail.com>
2020-11-10 00:04:12 -0800

    Hello to the Individual in React (not the world)

--- src/rum_workshop/scratch.cljs
+++ src/rum_workshop/scratch.cljs
@@ -3,10 +3,28 @@
 
 (enable-console-print!)
 
+(def enter-key-code 13)
+
+(def my-name (atom ""))
+
+(def db (atom {:input-text ""}))
+
+(def input-text (rum/cursor-in db [:input-text]))
+
 (def root-element (.getElementById js/document "app"))
 
 (rum/defc app < rum/reactive
   []
-  [:div "Hello World"])
+  [:div 
+   [:input 
+    {:value (rum/react input-text)
+     :on-change (fn [e] (reset! input-text (.. e -target -value)))
+     :on-key-down 
+     (fn [e] 
+       (js/console.log (.-keyCode e) @input-text)
+       (when (and (= (.-keyCode e) enter-key-code)
+               (seq @input-text))        
+         (reset! input-text "")))}]
+   [:p (str "Hello, "@input-text)]])
 
 (rum/mount (app) root-element)
