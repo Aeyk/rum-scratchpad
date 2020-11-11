04f12f104a7dbe30507cf3336d81ba4714d83493
Malik Kennedy <mksybr@gmail.com>
2020-11-11 14:45:51 -0800

    WebMIDI Hello Beep (chrome only :( )

--- src/rum_workshop/music.cljs
+++ src/rum_workshop/music.cljs
@@ -1,15 +1,25 @@
 (ns rum-workshop.music
   (:require [rum.core :as rum :refer [adapt-class]]
             ["tone" :as tone :default Synth]
-            ["@tonejs/piano" :as piano :default Piano]))
+            ["@tonejs/piano" :as piano :default Piano]
+            ["webmidi" :as webmidi :default WebMidi]))
 
 (enable-console-print!)
 
 (def root-element (.getElementById js/document "app"))
 
+(defn init-webmidi []
+  (.enable webmidi
+    (fn [err]
+      (if err
+        (js/console.log "WebMidi could not be enabled.", err)
+        (js/console.log "WebMidi enabled!"))
+      )))
+
 (rum/defc app < 
   {:did-mount 
    (fn [e]
+     (init-webmidi)
      (js/console.log tone)
      (.triggerAttackRelease 
        (.toDestination (new tone/Synth.))
