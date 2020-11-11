38ddafa96858302fb727c92684363637bc8b51e6
Malik Kennedy <mksybr@gmail.com>
2020-11-11 14:46:44 -0800

    Rmove WebMidi

--- src/rum_workshop/music.cljs
+++ src/rum_workshop/music.cljs
@@ -1,25 +1,15 @@
 (ns rum-workshop.music
   (:require [rum.core :as rum :refer [adapt-class]]
             ["tone" :as tone :default Synth]
-            ["@tonejs/piano" :as piano :default Piano]
-            ["webmidi" :as webmidi :default WebMidi]))
+            ["@tonejs/piano" :as piano :default Piano]))
 
 (enable-console-print!)
 
 (def root-element (.getElementById js/document "app"))
 
-(defn init-webmidi []
-  (.enable webmidi
-    (fn [err]
-      (if err
-        (js/console.log "WebMidi could not be enabled.", err)
-        (js/console.log "WebMidi enabled!"))
-      )))
-
 (rum/defc app < 
   {:did-mount 
    (fn [e]
-     (init-webmidi)
      (js/console.log tone)
      (.triggerAttackRelease 
        (.toDestination (new tone/Synth.))
