1cac782fb486f4aa95dfd9fa257de4ee147fb464
Malik Kennedy <mksybr@gmail.com>
2020-11-11 14:32:30 -0800

    Dependencies, node shadow-cljs.end compiler options

--- src/rum_workshop/music.cljs
+++ src/rum_workshop/music.cljs
@@ -1,16 +1,21 @@
 (ns rum-workshop.music
-  (:require [rum.core :as rum]))
+  (:require [rum.core :as rum :refer [adapt-class]]
+            ["tone" :as tone]))
 
 (enable-console-print!)
 
 (def root-element (.getElementById js/document "app"))
 
-(rum/defc app < rum/reactive
-  []
-  [:div 
-   "Hello world"])
+(rum/defcs app < rum/reactive
+  [state _ _]
+  {:did-mount 
+   (fn [e]
+     (js/console.log tone))}
+   )
 
-(defn refresh [] (rum/mount (app) root-element))
+(defn refresh [] 
+  (rum/mount 
+    (app) root-element))
 
 (defn ^:export init []
   (refresh))
