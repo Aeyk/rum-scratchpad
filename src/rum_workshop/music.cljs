f0c9460405bd184d469da3a141fffcc5927f5ebc
Malik Kennedy <mksybr@gmail.com>
2020-11-11 19:52:02 -0800

    More and more options: more and more NOISE

--- src/rum_workshop/music.cljs
+++ src/rum_workshop/music.cljs
@@ -40,28 +40,130 @@
     (js/console.log chord)))
 
 (def four-random-loop
-  #js [#js ["D4" "F#4" "A4"]
-       #js ["C4" "E4" "G4"]
-       #js ["G4" "B4" "D4"]
-       #js ["C4" "E4" "G4"]])
+  (atom 
+    #js [#js ["D4" "F#4" "A4"]
+         #js ["C4" "E4" "G4"]
+         #js ["G4" "B4" "D4"]
+         #js ["C4" "E4" "G4"]]))
+
+(defn cycle-chords-loop []
+  (set! four-random-loop 
+    (conj (rest @four-random-loop) (first @four-random-loop)))
+  four-random-loop)
 
 (def sample (comp first shuffle))
 
+(defn transpose [notes delta]
+  (apply array 
+    (map #(as-> % $$ 
+            (tone/Midi. $$) 
+            (.transpose $$ delta))
+      notes)))
+
 (defn init-clock []
   (set-bpm 200)
-  (.start 
-    (new tone/Pattern
-      (fn [time note]
-        (js/console.log note)
-        (set! this .-humanize true)
-        (play-note note (sample ["2n" "4n" "8n" "16n" "16n" "32n" "32n" "32n" "32n" ])))
-      four-random-loop
-      "up"))
   (.start tone/Transport))
 
+
 (defn init-keybinding []
+  (def s (atom 0)) 
+
+  (key/bind! "f" ::my-trigger 
+    #(do 
+       (swap! s + 7)
+      (js/console.log @s)))
+
+
+  (key/bind! "g" ::my-trigger 
+    #(do 
+       (swap! s - 5)
+      (js/console.log @s)))
+
+
+  (key/bind! "q" ::my-trigger 
+    #(play-note
+       (transpose #js ["D4" "F#4" "A4"]  (+ 0 @s))
+       "16n"))
+  (key/bind! "w" ::my-trigger 
+    #((comp js/console.log play-note) 
+      (transpose #js ["D4" "F#4" "A4"] (+ 5 @s))
+      "16n"))
+  (key/bind! "e" ::my-trigger 
+    #((comp js/console.log play-note) 
+      (transpose #js ["D4" "F#4" "A4"] (+ 7 @s))
+      "16n"))
+
   (key/bind! "c" ::my-trigger 
-    play-random-chord))
+    #(play-note
+       (transpose #js ["C4" "E4" "G4"]  (+ 0 @s))
+       "16n"))
+  (key/bind! "x" ::my-trigger 
+    #((comp js/console.log play-note) 
+      (transpose #js ["C4" "E4" "G4"] (+ 5 @s))
+      "16n"))
+  (key/bind! "z" ::my-trigger 
+    #((comp js/console.log play-note) 
+      (transpose #js ["C4" "E4" "G4"] (+ 7 @s))
+      "16n"))
+
+  (key/bind! "a" ::my-trigger 
+    #((comp js/console.log play-note) 
+      (transpose #js ["E4" "B4" "G4"] (+ 2 @s))
+      "16n"))
+
+  (key/bind! "s" ::my-trigger 
+    #((comp js/console.log play-note) 
+      (transpose #js ["E4" "G4" "B4"] (+ 3 @s))
+      "16n"))
+  (key/bind! "d" ::my-trigger 
+    #((comp js/console.log play-note) 
+      (transpose #js ["E4" "G4" "B4"] (+ 4 @s))
+      "16n"))
+
+  (key/bind! "h" ::my-trigger 
+    #((comp js/console.log play-note) 
+      (transpose (array (sample #js ["E4" "B4" "G4"])) (+ 2 @s))
+      "16n"))
+  (key/bind! "j" ::my-trigger 
+    #((comp js/console.log play-note) 
+      (transpose (array (sample #js ["E4" "B4" "G4"])) (+ 3 @s))
+      "16n"))
+  (key/bind! "k" ::my-trigger 
+    #((comp js/console.log play-note) 
+      (transpose (array (sample #js ["E4" "B4" "G4"])) (+ 4 @s))
+      "16n"))
+
+  (key/bind! "y" ::my-trigger 
+    #(play-note
+       (transpose (array (sample  #js ["D4" "F#4" "A4"]))  (+ 0 @s))
+       "16n"))
+  (key/bind! "u" ::my-trigger 
+    #((comp js/console.log play-note) 
+      (transpose (array (sample #js ["D4" "F#4" "A4"])) (+ 5 @s))
+      "16n"))
+  (key/bind! "i" ::my-trigger 
+    #((comp js/console.log play-note) 
+      (transpose (array (sample #js ["D4" "F#4" "A4"])) (+ 7 @s))
+      "16n"))
+
+  (key/bind! "n" ::my-trigger 
+    #(play-note
+       (transpose (array (sample #js ["C4" "E4" "G4"]))  (+ 0 @s))
+       "16n"))
+  (key/bind! "m" ::my-trigger 
+    #((comp js/console.log play-note) 
+      (transpose (array (sample #js ["C4" "E4" "G4"])) (- 5 @s))
+      "16n"))
+  (key/bind! "," ::my-trigger 
+    #((comp js/console.log play-note) 
+      (transpose (array (sample #js ["C4" "E4" "G4"])) (- 7 @s))
+      "16n"))
+  
+  
+
+  
+  )
+
 
 (rum/defc app < 
   {:did-mount 
@@ -70,7 +172,12 @@
      (init-keybinding)
      (init-clock))}
   []
-  [:div "Hello"] )
+  [:div 
+   [:pre "QWE ASD ZXC for chords"] 
+   [:pre "YUI HJK NM, for melody"]
+   [:pre "F G for transpose"]
+   
+   ] )
 
 (defn refresh [] 
   (rum/mount 
