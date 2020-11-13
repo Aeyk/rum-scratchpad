f3d41b5f57c9b07ffe955986815e4b422e849202
Malik Kennedy <mksybr@gmail.com>
2020-11-12 19:40:29 -0800

    Copy template to etebase

---
+++ src/rum_workshop/etebase.cljs
@@ -0,0 +1,16 @@
+(ns rum-workshop.etebase
+  (:require [rum.core :as rum]))
+
+(enable-console-print!)
+
+(def root-element (.getElementById js/document "app"))
+
+(rum/defc app < rum/reactive
+  []
+  [:div 
+   "Hello world"])
+
+(defn refresh [] (rum/mount (app) root-element))
+
+(defn ^:export init []
+  (refresh))
