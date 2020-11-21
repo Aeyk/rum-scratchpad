dbd15c82cff9208119673ed0913f46ca96b1cf14
Malik Kennedy <mksybr@gmail.com>
2020-11-21 12:30:25 -0800

    Added esri-leaflet for easy basemap

--- src/rum_workshop/map.cljs
+++ src/rum_workshop/map.cljs
@@ -2,19 +2,31 @@
   (:require [rum.core :as rum] 
             ;; https://shadow-cljs.github.io/docs/UsersGuide.html#_conditional_reading
             ;; shadow-cljs example for import libraries
-            ["react-leaflet" :as react-leaflet]))
+            ["react-leaflet" :as react-leaflet]
+            ["leaflet" :as leaflet]
+            ["esri-leaflet" :as esri-leaflet]))
 ;; https://gist.github.com/orolle/21902cc4a58f2a97c7ab27c2fb7b2503
 
 (enable-console-print!)
 
 (def root-element (.getElementById js/document "app"))
-
+(def position (atom [51.505, -0.09]))
 (rum/defc app < rum/reactive
+  {:did-mount
+   (fn [state]
+     #_(js/console.log react-leaflet/MapContainer)
+     (.addTo
+      (esri-leaflet/basemapLayer "Gray")
+      (.setView
+       (leaflet/map "mapid" {:preferCanvas true})
+       #js [51.505, -0.09], 13)) )}
   []
-  [:div 
-   "Hello world"])
+  [:div#mapid
+   ]
+   )
 
-(defn refresh [] (rum/mount (app) root-element))
+(defn refresh []
+  (rum/mount (app) root-element))
 
 (defn ^:export init []
   (refresh))
