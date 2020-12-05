da468a60a89f9235ba6e5ce417af082186643629
Malik Kennedy <mksybr@gmail.com>
2020-12-05 02:43:49 -0800

    feat: Add placeholder keybindings for space + arrows

--- src/rum_workshop/parallax.cljs
+++ src/rum_workshop/parallax.cljs
@@ -1,10 +1,28 @@
-(ns rum-workshop.parallax)
+(ns rum-workshop.parallax
+  (:require [keybind.core :as key]))
 
 (def canvas (js/document.getElementById "canvas"))
 (def context (.getContext canvas "2d"))
 
+(defn init-keybindings []
+  (letfn [(left []
+            (js/console.log "LEFT"))
+          (right []
+            (js/console.log "RIGHT"))
+          (up []
+            (js/console.log "UP"))
+          (down []
+            (js/console.log "DOWN"))
+          (space []
+            (js/console.log"SPACE"))]
+    (key/bind! "space" ::space space)
+    (key/bind! "left"  ::left  left)
+    (key/bind! "right" ::right right)
+    (key/bind! "up"    ::up    up)
+    (key/bind! "down"  ::down  down)))
 
 (defn ^:export init []
+  (init-keybindings)
   (set! context.fillStyle "green")
   (.fillRect context 10 10 150 150))
 
