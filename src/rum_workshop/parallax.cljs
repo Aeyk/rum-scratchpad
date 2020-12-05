f1c1475d83a8f361f2ebe28a0240c912aaddbf05
Malik Kennedy <mksybr@gmail.com>
2020-12-05 02:32:43 -0800

    bugfix+chore: Fix ns declaration, + js canvas api template

--- src/rum_workshop/parallax.cljs
+++ src/rum_workshop/parallax.cljs
@@ -1,1 +1,8 @@
-(ns rum-scratchpad.parallax)
+(ns rum-workshop.parallax)
+
+(def canvas (js/document.getElementById "canvas"))
+(def context (js/canvas.getContext "2d"))
+
+(js/context.fillStyle "green")
+(js/context.fillRect 10 10 150 150)
+
