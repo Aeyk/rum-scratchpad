22b3af7778d9ea42d932ee3a0aff0df94af057b5
Malik Kennedy <mksybr@gmail.com>
2020-12-04 21:09:56 -0800

    chore: Add dependencies in preperation for playing with logic

--- src/rum_workshop/music.cljs
+++ src/rum_workshop/music.cljs
@@ -1,5 +1,8 @@
 (ns rum-workshop.music
-  (:require [rum.core :as rum :refer [adapt-class]]
+  (:require [clojure.core.logic :refer :all]
+            [clojure.core.logic.fd :as fd]
+            [rum.core :as rum :refer [adapt-class]]
+            [music-theory.note :as note]
             [keybind.core :as key]
             ["tone" :as tone]))
 
