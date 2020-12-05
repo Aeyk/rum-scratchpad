61028f5646c55feff68668bc7aabf1f6240be89f
Malik Kennedy <mksybr@gmail.com>
2020-12-04 21:11:45 -0800

    feat: Sandbox area to play with BaaS etebase

--- src/rum_workshop/etebase.cljs
+++ src/rum_workshop/etebase.cljs
@@ -1,7 +1,16 @@
 (ns rum-workshop.etebase
-  (:require [rum.core :as rum]))
+  (:require [rum.core :as rum]
+            ["etebase" :as etebase]))
 
 (enable-console-print!)
+
+(def etebase-url "https://api.etebase.com/developer/mksybr/")
+
+(defn init-etebase-account []
+  (js/console.log 
+    (.then
+      (etebase/Account.login "mksybr" "FzqNFBzg5uoEt3cKgHT64A" etebase-url)
+      (fn [e] (js/console.log e)))))  
 
 (def root-element (.getElementById js/document "app"))
 
@@ -10,7 +19,9 @@
   [:div 
    "Hello world"])
 
-(defn refresh [] (rum/mount (app) root-element))
+(defn refresh [] 
+  (init-etebase-account)
+  (rum/mount (app) root-element))
 
 (defn ^:export init []
   (refresh))
--- src/rum_workshop/server.clj
+++ src/rum_workshop/server.clj
@@ -1,15 +1,18 @@
 (ns rum-workshop.server
-  (:require [org.httpkit.server :as http]
-            
+  (:require [org.httpkit.server :as http]            
             [taoensso.sente :as sente]            
             [taoensso.sente.server-adapters.http-kit 
              :refer (get-sch-adapter)]
             [clojure.core.async :as async  
-             :refer (<! <!! >! >!! put! chan go go-loop)]))
+             :refer (<! <!! >! >!! put! chan go go-loop)]
+            [ring.middleware.cors :refer [wrap-cors]]))
 
 (defn baby-small-app [req] 
   {:status  200
    :headers {"Content-Type" "text/html"}
-   :body    "Pew pew!"})  
+   :body    (slurp "public/index.html")})  
 
-(server/run-server #'baby-small-app {:port 8888})
+(http/run-server (wrap-cors #'baby-small-app)
+  :access-control-allow-origin [#"https://api.etebase.com"]
+  :access-control-allow-methods [:get :put :post :delete]
+                       {:port 8888})
