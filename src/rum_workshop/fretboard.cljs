c53a4f3d40748d35324673d59f7d531d871ef158
Malik Kennedy <mksybr@gmail.com>
2020-12-07 09:08:04 -0800

    chore: point leinigen to new logico-musical experimentation

---
+++ src/rum_workshop/fretboard.cljs
@@ -0,0 +1,8 @@
+(ns rum-workshop.fretboadr
+  (:require
+   [clojure.core.logic :refer :all]
+   [clojure.core.logic.fd :as fd]
+   [rum.core :as rum :refer [adapt-class]]
+   [music-theory.note :as note]
+   [keybind.core :as key]
+   ["tone" :as tone]))
