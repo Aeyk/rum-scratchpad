76f25a27691099f4b24217b576659de6e1b51167
Malik Kennedy <mksybr@gmail.com>
2021-01-01 02:49:08 -0800

    Show error message from backend

--- src/supabase.cljs
+++ src/supabase.cljs
@@ -5,10 +5,12 @@
 (def url "https://lftzhklytmxclipatzas.supabase.co")
 (def api-key "***REMOVED***")
  
-(def form-data (atom {:email "" :password "" :password_confirmation ""}))
+(def form-data (atom {:email "" :password ""
+                      :password_confirmation "" :notify ""}))
 (def email (rum/cursor-in form-data [:email]))
 (def password (rum/cursor-in form-data [:password]))
 (def password_confirmation (rum/cursor-in form-data [:password_confirmation]))
+(def notify (rum/cursor-in form-data [:notify]))
 
 (defn input
   ([name atom]
@@ -24,22 +26,41 @@
 (defn set-button-to-spinner [el]
   (.add (.-classList (js/document.querySelector el)) "is-loading"))
 
+(defn passwords-dont-match []
+  (reset! notify "Sorry the passwords don't match"))
+
+(defn error [e]
+  (js/setTimeout #(error "") 3000)
+  (reset! notify e))
+
+(defonce client (supabase/createClient url  api-key))
+
 (rum/defc app <
   rum/reactive
   []
-  {:will-mount
-   (fn [e]
-     (defonce client (supabase/createClient url  api-key))
-     )}
-  []
   [:div.card
+   [:p.notify
+    {:on-change passwords-dont-match}
+    (str (rum/react notify))]
    [:form
     {:on-submit
      (fn [e]
        (.preventDefault e)
-       (set-button-to-spinner "#signup")
+       #_(set-button-to-spinner "#signup")
        (js/console.log
-        (map #(clj->js %) [email password password_confirmation])))}
+        (clj->js [@email @password @password_confirmation]))
+       (if-not (== @password @password_confirmation)
+         (passwords-dont-match)
+         (.then
+          (.signUp client.auth
+                   #js
+                   {:email @email
+                    :password @password})
+          (fn [res]
+            (error res.error)
+            (js/console.log
+             res.error)))
+         ))}
     (input "Email" email)
     (input "Password" password)
     (input "Password Confirmation" password_confirmation)
